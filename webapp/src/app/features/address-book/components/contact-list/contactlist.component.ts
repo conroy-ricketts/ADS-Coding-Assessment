ad0752c4a6cdbbf6ac11382b862bfc556d553c4f
Conroy Ricketts <conroy.ricketts@tutanota.com>
2025-05-16 11:50:07 -0400

    Added basic CRUD operations for the contact list.

--- webapp/src/app/features/address-book/components/contact-list/contactlist.component.ts
+++ webapp/src/app/features/address-book/components/contact-list/contactlist.component.ts
@@ -10,29 +10,75 @@ import { Contact, ContactService } from '../../contact.service'
   template: `
     <h1>Contacts</h1>
     <input [(ngModel)]="queryString" placeholder="Filter by either contact name or company name" />
+    <button (click)="showAddForm = !showAddForm">{{ showAddForm ? 'Cancel' : 'Add Contact' }}</button>
+    <div *ngIf="showAddForm">
+      <h3>Add Contact</h3>
+      <form (ngSubmit)="addContact()">
+        <strong>Required Fields</strong><br>
+        <input [(ngModel)]="newContact.ContactName" name="ContactName" placeholder="Name" required /><br>
+        <input [(ngModel)]="newContact.CustomerID" name="CustomerID" placeholder="Customer ID" required /><br>
+        <strong>Optional Fields</strong><br>
+        <input [(ngModel)]="newContact.CompanyName" name="CompanyName" placeholder="Company Name" /><br> 
+        <input [(ngModel)]="newContact.Address" name="Address" placeholder="Address" /><br>
+        <input [(ngModel)]="newContact.City" name="City" placeholder="City" /><br>
+        <input [(ngModel)]="newContact.Country" name="Country" placeholder="Country" /><br>
+        <input [(ngModel)]="newContact.Email" name="Email" placeholder="Email" /><br>
+        <input [(ngModel)]="newContact.Phone" name="Phone" placeholder="Phone" /><br>
+        <input [(ngModel)]="newContact.Fax" name="Fax" placeholder="Fax" /><br>
+        <input [(ngModel)]="newContact.Region" name="Region" placeholder="Region" /><br>
+        <input [(ngModel)]="newContact.ContactTitle" name="ContactTitle" placeholder="Title" /><br>
+        <input [(ngModel)]="newContact.PostalCode" name="PostalCode" placeholder="Postal Code" /><br>
+        <button type="submit">Save</button>
+      </form>
+    </div>
     <body *ngFor="let contact of filteredContacts()">
-      <h2>{{ contact.ContactName }}</h2>
-      <strong>Company:</strong> {{ contact.CompanyName }}<br>
-      <strong>Address:</strong> {{ contact.Address }}<br>
-      <strong>City:</strong> {{ contact.City }}<br>
-      <strong>Country:</strong> {{ contact.Country }}<br>
-      <strong>Email:</strong> {{ contact.Email }}<br>
-      <strong>Phone:</strong> {{ contact.Phone }}<br>
-      <strong>Fax:</strong> {{ contact.Fax }}<br>
-      <strong>Region:</strong> {{ contact.Region }}<br>
-      <strong>Customer ID:</strong> {{ contact.CustomerID }}<br>
-      <strong>Contact Title:</strong> {{ contact.ContactTitle }}<br>
-      <strong>Postal Code:</strong> {{ contact.PostalCode }}<br>
+      <div *ngIf="editID !== contact.CustomerID">
+        <h2>{{ contact.ContactName }}</h2>
+        <strong>Company:</strong> {{ contact.CompanyName }}<br>
+        <strong>Address:</strong> {{ contact.Address }}<br>
+        <strong>City:</strong> {{ contact.City }}<br>
+        <strong>Country:</strong> {{ contact.Country }}<br>
+        <strong>Email:</strong> {{ contact.Email }}<br>
+        <strong>Phone:</strong> {{ contact.Phone }}<br>
+        <strong>Fax:</strong> {{ contact.Fax }}<br>
+        <strong>Region:</strong> {{ contact.Region }}<br>
+        <strong>Customer ID:</strong> {{ contact.CustomerID }}<br>
+        <strong>Contact Title:</strong> {{ contact.ContactTitle }}<br>
+        <strong>Postal Code:</strong> {{ contact.PostalCode }}<br>
+        <button (click)="startEdit(contact)">Edit</button>
+        <button (click)="deleteContact(contact.CustomerID)">Delete</button>
+      </div>
+      <div *ngIf="editID === contact.CustomerID">
+        <form (ngSubmit)="updateContact()">
+        <strong>Required Fields</strong><br>
+          <input [(ngModel)]="editContact.ContactName" name="editContactName" placeholder="Name" required /><br>
+          <input [(ngModel)]="editContact.CustomerID" name="editCustomerID" placeholder="Customer ID" required /><br>
+          <strong>Optional Fields</strong><br>
+          <input [(ngModel)]="editContact.CompanyName" name="editCompanyName" placeholder="Company Name" /><br> 
+          <input [(ngModel)]="editContact.Address" name="editAddress" placeholder="Address" /><br>
+          <input [(ngModel)]="editContact.City" name="editCity" placeholder="City" /><br>
+          <input [(ngModel)]="editContact.Country" name="editCountry" placeholder="Country" /><br>
+          <input [(ngModel)]="editContact.Email" name="editEmail" placeholder="Email" /><br>
+          <input [(ngModel)]="editContact.Phone" name="editPhone" placeholder="Phone" /><br>
+          <input [(ngModel)]="editContact.Fax" name="editFax" placeholder="Fax" /><br>
+          <input [(ngModel)]="editContact.Region" name="editRegion" placeholder="Region" /><br>
+          <input [(ngModel)]="editContact.ContactTitle" name="editContactTitle" placeholder="Title" /><br>
+          <input [(ngModel)]="editContact.PostalCode" name="editPostalCode" placeholder="Postal Code" /><br>
+          <button type="submit">Update</button>
+          <button type="button" (click)="editID = null">Cancel</button>
+        </form>
+      </div>
     </body>
   `
 })
 
 export class ContactListComponent implements OnInit {
-  // Store a list of contacts retrieved from the contact service.
   contacts: Contact[] = []
-
-  // Store the filter query string for searching the list of contacts.
-  queryString = ''
+  queryString: string = ''
+  showAddForm: boolean = false
+  newContact: Contact = {} as Contact
+  editID: string | null = null
+  editContact: Contact = {} as Contact
 
   constructor(private contactService: ContactService) { }
 
@@ -50,5 +96,29 @@ export class ContactListComponent implements OnInit {
       contact.ContactName.toLowerCase().includes(searchConstraint) ||
       contact.CompanyName.toLowerCase().includes(searchConstraint)
     )
+  }
+
+  // Method to add a new contact to the list.
+  addContact() {
+    this.contactService.addContact({ ...this.newContact })
+    this.showAddForm = false
+    this.newContact = {} as Contact
+  }
+
+  // Method to update an existing contact in the list.
+  updateContact() {
+    this.contactService.updateContact(this.editContact)
+    this.editID = null
+  }
+
+  // Method to start editing a contact.
+  startEdit(contact: Contact) {
+    this.editID = contact.CustomerID;
+    this.editContact = { ...contact };
+  }
+
+  // Method to delete a contact from the list (by customer ID).
+  deleteContact(customerID: string) {
+    this.contactService.deleteContact(customerID)
   }
 }
--- webapp/src/app/features/address-book/contact.service.ts
+++ webapp/src/app/features/address-book/contact.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
-import { Observable } from 'rxjs'
+import { BehaviorSubject, Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 
 // Note that some contacts may not have a fax or region.
@@ -19,16 +19,19 @@ export interface Contact {
   Region?: string
 }
 
-// This service is responsible for fetching the source list of contact data from the XML file.
+// This service is responsible for fetching the source list of contact data from the XML file, and basic CRUD operations.
 @Injectable({ providedIn: 'root' })
 export class ContactService {
+  private contactsBehaviorSubject = new BehaviorSubject<Contact[]>([])
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+    this.loadContacts()
+  }
 
-  // Method to create an observable which fetches the XML file and parses it into a list of contacts.
-  getContacts(): Observable<Contact[]> {
+  // Method to fetch the source XML file and parse it into a list of contacts.
+  loadContacts() {
     // Load the raw XML file as a string.
-    return this.httpClient.get('assets/ab.xml', { responseType: 'text' }).pipe(
+    this.httpClient.get('assets/ab.xml', { responseType: 'text' }).pipe(
       // Map the raw XML string to a list of contacts.
       map(xmlStr => {
         const parser = new DOMParser()
@@ -55,6 +58,33 @@ export class ContactService {
 
         return contacts
       })
+    ).subscribe(contacts => this.contactsBehaviorSubject.next(contacts))
+  }
+
+  // Method to get the current list of contacts as an observable.
+  getContacts(): Observable<Contact[]> {
+    return this.contactsBehaviorSubject.asObservable()
+  }
+
+  // Method to add a new contact to the list.
+  addContact(contact: Contact) {
+    const contacts = this.contactsBehaviorSubject.getValue()
+    this.contactsBehaviorSubject.next([contact, ...contacts])
+  }
+
+  // Method to update an existing contact in the list (by customer ID).
+  updateContact(updatedContact: Contact) {
+    const contacts = this.contactsBehaviorSubject.getValue().map(contact =>
+      contact.CustomerID === updatedContact.CustomerID ? updatedContact : contact
     )
+    this.contactsBehaviorSubject.next(contacts)
+  }
+
+  // Method to delete a contact from the list (by customer ID).
+  deleteContact(customerId: string) {
+    const contacts = this.contactsBehaviorSubject.getValue().filter(contact =>
+      contact.CustomerID !== customerId
+    )
+    this.contactsBehaviorSubject.next(contacts)
   }
 }
