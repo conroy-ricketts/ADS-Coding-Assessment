5a537b2119eb793558c2e76949e183751a27942e
Conroy Ricketts <conroy.ricketts@tutanota.com>
2025-05-16 08:57:51 -0400

    Initialized Angular WebApp, connecting it to Android project WebView.

--- app/src/main/java/com/example/adscodingassessment/MainActivity.kt
+++ app/src/main/java/com/example/adscodingassessment/MainActivity.kt
@@ -1,47 +1,63 @@
 package com.example.adscodingassessment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.webkit.WebViewAssetLoader
 import com.example.adscodingassessment.ui.theme.ADSCodingAssessmentTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Stretch the app to fill the entire screen.
         enableEdgeToEdge()
+
         setContent {
             ADSCodingAssessmentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // This WebView will contain the majority of the app's content.
+                WebView()
             }
         }
     }
 }
 
+@SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun WebView() {
+    AndroidView(factory = { context ->
+        // Create an instance of WebViewAssetLoader.
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(context))
+            .addPathHandler("/res/", WebViewAssetLoader.ResourcesPathHandler(context))
+            .build()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ADSCodingAssessmentTheme {
-        Greeting("Android")
-    }
+        WebView(context).apply {
+            settings.javaScriptEnabled = true
+            settings.allowFileAccess = true
+            settings.domStorageEnabled = true
+
+            // Intercept requests and use the instance of WebViewAssetLoader.
+            webViewClient = object : WebViewClient() {
+                override fun shouldInterceptRequest(
+                    view: WebView,
+                    request: WebResourceRequest
+                ): WebResourceResponse? {
+                    return assetLoader.shouldInterceptRequest(request.url)
+                }
+            }
+
+            // Load the initial URL using the same https scheme as the WebViewAssetLoader.
+            // Note that "appassets.androidplatform.net" is a conventional placeholder.
+            loadUrl("https://appassets.androidplatform.net/assets/webapp/index.html")
+        }
+    })
 }
